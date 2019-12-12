f40e0c08a2de8d1e18dcb88ae2f700e59ac977fb
Sotiris <sotiriskarageorgopoulos@gmail.com>
2019-12-12 22:52:45 +0200

    3.7: Phonebook backend step7 & 3.8*: Phonebook backend step8

--- part3/backend_phonebook/index.js
+++ part3/backend_phonebook/index.js
@@ -1,7 +1,19 @@
 const express = require('express')
-const app = express()
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const app = express()
+
+app.use(morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),' ',
+    tokens.url(req, res),' ',
+    tokens.status(req, res),' ',
+    tokens.res(req, res, 'content-length'), ' - ',
+    tokens['response-time'](req, res), ' ms ',JSON.stringify(req.body)
+  ].join('')
+}))
 app.use(bodyParser.json())
+
 
 let persons = [
     {
