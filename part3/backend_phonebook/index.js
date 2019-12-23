17942c789fe77ec7b561cf6b389088d99030540b
Sotiris <sotiriskarageorgopoulos@gmail.com>
2019-12-24 00:57:30 +0200

    3.16: honebook database, step3

--- part3/backend_phonebook/index.js
+++ part3/backend_phonebook/index.js
@@ -95,6 +95,24 @@ app.delete('/api/persons/:id', (req, res, next) => {
     .catch(error => next(error))
 })
 
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError' && error.kind == 'ObjectId') {
+    return res.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
 const PORT = process.env.PORT
 
 app.listen(PORT, () => {
