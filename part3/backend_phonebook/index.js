ce9991ec2069a6f423ff57d1725d28b5feb8b3ca
Sotiris <sotiriskarageorgopoulos@gmail.com>
2019-12-24 00:38:59 +0200

    3.13: honebook database, step1 & 3.14: honebook database, step2

--- part3/backend_phonebook/index.js
+++ part3/backend_phonebook/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const app = express()
+require('dotenv').config()
+const phoneBook = require('./models/phonebook')
 
 app.use(express.static('build'))
 app.use(morgan(function (tokens, req, res) {
@@ -45,60 +47,29 @@ let persons = [
   ]
 
   app.get('/api/persons',(req,res) => {
-      res.json(persons)
+    phoneBook.find({}).then(p => {
+        res.json(p)
+    }).catch(error=>console.log(error))
   })
 
   app.get('/api/persons/:id',(req,res) => {
-      const id = Number(req.params.id)
-      let person = persons.find(p=>p.id === id)
-      if(person){
-          res.json(person)
-      }
-      else{
-          res.status(404).end()
-      }
+      phoneBook.findById(req.params.id).then(p => {
+        res.json(p)
+      }).catch(error=>console.log(error))
   })
 
-  app.get('/info',(req,res) => {
-     const requestTime = new Date().toUTCString()
-     let entriesInfo = `Phonebook has info for ${persons.length} people`
-     res.send(`<p>${entriesInfo}</p><p>${requestTime}</p>`) 
-  })
-
-  app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(note => note.id !== id)
-    response.status(204).end()
-  }) 
-
-  
   app.post('/api/persons',(req,res) => {
-      let bodyReq = req.body
-      let isExistsName = persons.filter(p => p.name.toLowerCase() === bodyReq.name.toLowerCase() 
-                                       || p.name.toUpperCase() === bodyReq.name.toUpperCase())
-      if((bodyReq.name.trim().length === 0 || bodyReq.number.trim().length === 0) && isExistsName.length === 0) {
-        res.status(400).json({error: "name is missing or number is missing or both are missing!!!"})
-      }
-      else if(isExistsName.length !== 0) {
-        res.status(400).json({error: "The name already exists in the phonebook!!!"})
-      }
-      else {
-      const person = {
-        name: bodyReq.name,
-        number: bodyReq.number,
-        id: generateId()
-      }
-      persons = persons.concat(person)
-      res.json(person)
-     }
+      const person = new phoneBook({
+        name: req.body.name,
+        number: req.body.number
+      })
+      person.save().then( p =>{
+         res.json(p)
+      }).catch(error=>console.log(error))
   })
 
-  const PORT = process.env.PORT || 3001
+  const PORT = process.env.PORT
 
   app.listen(PORT,() => {
       console.log(`Server running on port: ${PORT}`)
   })
-
-  const generateId = () => {
-    return Math.floor(Math.random()*100000000)
-  }
