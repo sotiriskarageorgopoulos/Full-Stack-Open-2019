21e3d3aed6119422841ef5272ebdd76b01b263c1
Sotiris <sotiriskarageorgopoulos@gmail.com>
2019-12-28 16:53:58 +0200

    4.5*: helper functions and unit tests, step3

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -6,7 +6,13 @@ const totalLikes = (blog) => {
    return blog.reduce((totalLikes,blog) => totalLikes + blog.likes,0)
 }
 
+const favouriteBlog = (blogs) => {
+  if(blogs.length === 0) return null
+  return blogs.reduce((favouriteBlog,blog)=>favouriteBlog.likes > blog.likes?favouriteBlog:blog,blogs[0])
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favouriteBlog
 }
