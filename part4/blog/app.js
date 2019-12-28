c136e884e0f3883fcdd44b406d81e489501909c4
Sotiris <sotiriskarageorgopoulos@gmail.com>
2019-12-28 15:23:40 +0200

    4.1 Blog list, step1 & 4.2 Blog list, step2

---
+++ part4/blog/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogRouter = require('../blog/controllers/blog')
+const config = require('../blog/utils/config')
+
+console.log('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connection to MongoDB:', error.message)
+  })
+
+app.use(cors)
+app.use(bodyParser.json())
+app.use('/api/blogs',blogRouter)
+
+module.exports = app
